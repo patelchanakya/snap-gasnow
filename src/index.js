413ffca2296135fe21aed9b03633c2959815a3f1
Chanakya <chanakya1patel@gmail.com>
2022-04-19 12:40:51 -0400

    tutorial

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
+async function getFees() {
+  let response = await fetch('https://etherchain.org/api/gasnow');
+  return response.text();
+}
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
     case 'hello':
+      const metadata = JSON.parse(await getFees());
+      const fees = metadata.data;
+      const standardprice = (fees.standard / 1000000000).toString();
+      const slowprice = (fees.slow / 1000000000).toString();
+      const fastprice = (fees.fast / 1000000000).toString();
+      const rapidprice = (fees.rapid / 1000000000).toString();
+      const ethprice = fees.priceUSD.toString();
+      const timestamp = new Date(fees.timestamp * 1000);
+
       return wallet.request({
         method: 'snap_confirm',
         params: [
           {
-            prompt: `Hello, ${originString}!`,
+            prompt: `Hello, gas fees`,
             description:
               'This custom confirmation is just for display purposes.',
             textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+              `At this current time of  ${timestamp} gas fees are as folows:` +
+              '\n' +
+              '\n' +
+              `Standard Gas Price is` +
+              `\n` +
+              `${standardprice} GWei` +
+              '\n' +
+              `For fast transactions:` +
+              `\n` +
+              `${fastprice} GWei` +
+              '\n' +
+              `For faster transactions:` +
+              `\n` +
+              `${rapidprice} GWei` +
+              '\n' +
+              `For slow transactions:` +
+              `\n` +
+              `${slowprice} GWei` +
+              '\n' +
+              '\n' +
+              `Current price of Ethereum:` +
+              `\t` +
+              `\t` +
+              `${ethprice} USD`,
           },
         ],
       });
